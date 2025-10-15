4f8d60335cf0c32af3ddcf713b0158c4b83cad70
Bernardo Chamilet <bernardoboggionechamilet@gmail.com>
2025-10-15 00:14:17 -0300

    Criação/correção models dos 3 tipos de usuário

--- src/models/administradores.go
+++ src/models/administradores.go
@@ -8,18 +8,17 @@ import (
 	"github.com/badoux/checkmail"
 )
 
-type Cliente struct {
+type Administrador struct {
 	Id       int    `json:"id,omitempty"`
 	Nome     string `json:"nome,omitempty"`
-	Cpf      string `json:"cpf,omitempty"`
-	Endereco string `json:"endereco,omitempty"`
 	Telefone string `json:"telefone,omitempty"`
 	Email    string `json:"email,omitempty"`
 	Senha    string `json:"senha,omitempty"`
+	Galpao   int    `json:"galpao,omitempty"`
 }
 
 // Validar valida formato e tamanho dos dados, remove espaços em branco e criptografa a senha
-func (u Cliente) Validar() error {
+func (u Administrador) Validar() error {
 	u.Nome = strings.TrimSpace(u.Nome)
 	if len(u.Nome) < 2 {
 		return errors.New("nome deve ter pelo menos 2 caracteres")
@@ -40,7 +39,7 @@ func (u Cliente) Validar() error {
 }
 
 // ValidarEmail valida email
-func (u Cliente) ValidarEmail() error {
+func (u Administrador) ValidarEmail() error {
 	if erro := checkmail.ValidateFormat(u.Email); erro != nil {
 		return errors.New("email invalido")
 	}
@@ -48,7 +47,7 @@ func (u Cliente) ValidarEmail() error {
 }
 
 // ValidarNome valida tamanho do nome
-func (u Cliente) ValidarNome() error {
+func (u Administrador) ValidarNome() error {
 	u.Nome = strings.TrimSpace(u.Nome)
 	if len(u.Nome) < 2 {
 		return errors.New("Nome deve ter pelo menos 2 caractéres")
@@ -57,32 +56,9 @@ func (u Cliente) ValidarNome() error {
 }
 
 // ValidarLogin verifica se dados de login estão presentes
-func (u Cliente) ValidarLogin() error {
+func (u Administrador) ValidarLogin() error {
 	if u.Email == "" || u.Senha == "" {
 		return errors.New("campos faltando")
-	}
-	return nil
-}
-
-type RespostaLogin struct {
-	Id    int    `json:"id"`
-	Token string `json:"token"`
-}
-
-type Senhas struct {
-	SenhaAtual string `json:"senha_atual,omitempty"`
-	SenhaNova  string `json:"senha_nova,omitempty"`
-}
-
-// ValidarSenhas valida tamanho das senhas
-func (u *Senhas) ValidarSenhas() error {
-	u.SenhaAtual = strings.TrimSpace(u.SenhaAtual)
-	if len(u.SenhaAtual) < 2 {
-		return errors.New("senha atual incorreta")
-	}
-	u.SenhaNova = strings.TrimSpace(u.SenhaNova)
-	if len(u.SenhaNova) < 2 {
-		return errors.New("senha nova deve ter pelo menos 2 caracteres")
 	}
 	return nil
 }
